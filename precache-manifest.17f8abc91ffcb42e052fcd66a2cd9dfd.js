a0fe6c3065068f138d0a0fe9ab9c1736573b73a2
Mrbook <1985904621@qq.com>
2019-11-29 15:48:56 +0800

    1

---
+++ precache-manifest.17f8abc91ffcb42e052fcd66a2cd9dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8bbd9fee61bba622f196ca42b6a2648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c8e44b8f4542a2d2dd0",
+    "url": "/static/css/2.1370bc8e.chunk.css"
+  },
+  {
+    "revision": "627dbbda526f3a01df29",
+    "url": "/static/css/main.458f54d7.chunk.css"
+  },
+  {
+    "revision": "1c8e44b8f4542a2d2dd0",
+    "url": "/static/js/2.5e816b16.chunk.js"
+  },
+  {
+    "revision": "627dbbda526f3a01df29",
+    "url": "/static/js/main.7ce91be3.chunk.js"
+  },
+  {
+    "revision": "526de460b7244ca6b42c",
+    "url": "/static/js/runtime-main.d32d88cb.js"
+  }
+]);
